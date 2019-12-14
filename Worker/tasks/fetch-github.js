82e740e56cce548d448588f667b43465f9a35794
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2019-12-14 23:06:02 +0500

    - implemented filtering algorithm to filter junior level jobs from github job's api result

--- Worker/tasks/fetch-github.js
+++ Worker/tasks/fetch-github.js
@@ -1,4 +1,11 @@
 const fetch = require('node-fetch');
+const redis = require('redis');
+const { promisify } = require('util');
+
+const client = redis.createClient();
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+
 
 const baseURL = 'https://jobs.github.com/positions.json';
 
@@ -12,8 +19,27 @@ async function fetchGithub() {
         const jobs = await res.json();
         allJobs.push(...jobs);
         resultCount = jobs.length;
+        console.log('got', resultCount, 'jobs')
         onPage++;
     }
+
+    console.log('got', allJobs.length, 'jobs list')
+
+    // filter algo
+    const jrJobs = allJobs.filter(job => {
+        const jobTitle = job.title.toLowerCase();
+
+        // algo logic
+        const filterCondition = (jobTitle.includes('senior') || jobTitle.includes('manager') || jobTitle.includes('sr.') || jobTitle.includes('architect'));
+        return !filterCondition;
+    });
+
+    console.log('Filtered Jr. Jobs', jrJobs.length)
+
+    // Set in Redis
+    const success = await setAsync('github', JSON.stringify(jrJobs));
+
+    console.log({ success })
 }
 
 module.exports = fetchGithub;
