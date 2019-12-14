880e159b73ecdc5ab953e1823fa31d20c18d6637
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2019-12-14 22:07:29 +0500

    Implemented cron job using cron package to fetch jobs from github jobs api after every minute

---
+++ Worker/index.js
@@ -0,0 +1,5 @@
+const CronJob = require('cron').CronJob;
+const fetchGithub = require('./tasks/fetch-github');
+
+// Fetch GitHub Jobs
+new CronJob('* * * * *', fetchGithub, null, true, 'America/Los_Angeles');
